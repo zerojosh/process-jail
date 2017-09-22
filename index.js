b6f46a537674deb4962e1ff28e0103d5300393a7
zerojosh <zerojosh@gmail.com>
2017-09-21 23:17:40 -0700

    Update index.js

--- index.js
+++ index.js
@@ -85,7 +85,7 @@ controller.on('bot_channel_join', function (bot, message) {
     bot.reply(message, "I'm here!")
 });
 
-controller.hears('hello', 'direct_message', function (bot, message) {
+controller.hears('hello', 'message_received', function (bot, message) {
     bot.reply(message, 'Hello!');
 });
 
